6e5ed7ae02eb1a7b8e821b07f6b9fbb87eb1e918
Stijn Voss <svoss@i-sti.nl>
2018-03-11 18:47:25 +0100

    Included Kolmogorov-Smirnov implementation for convergence. New format of generator function for loop.

--- bequestlib/society_rules.py
+++ bequestlib/society_rules.py
@@ -31,6 +31,35 @@ def oldest_gets_all(children, b):
     return [b] + [0.]*(len(children)-1)
 
 
+def male_primogeniture(children, b):
+    """
+    Bequest rule. Give all planned bequest money to eldest son. If
+    no sons, the eldest daughter receives all
+
+    :param children: list of children. ``True`` for boy,
+        ``False`` for a girl
+    :type children: ``list`` of ``bool``
+    :param b: total amount of planned bequests
+    :type b: float
+    :return: list of bequests to each respective child
+    :rtype: ``list`` of ``float``
+    """
+    beq_vec = [0.]*len(children)
+    # if there are men among the children:
+    if sum(children) != 0:
+        first_found = False
+        for i, child in enumerate(children):
+            if child and not first_found:
+                # bequest all to eldest son
+                beq_vec[i] = b
+                first_found = True
+    # if there are no men among the children:
+    else:
+        # eldest daughter gets all
+        beq_vec[0] = b
+    return beq_vec
+
+
 def best_partner_is_richest_partner(bachelor_stats, bachelorette_stats):
     """
     Marital rule. Matches bachelors according to their wealth. Bachelors
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import os
 
-from bequestlib.couple import Couple
-from bequestlib.generation import Generation
-from bequestlib.globals import OUTFOLDER
-from bequestlib.metrics import lorentz_curve, gini, mobility_matrix
-from bequestlib.person import Person
-from bequestlib.society_rules import (all_children_equal,
-                                      love_is_blind, best_partner_is_richest_partner)
-from bequestlib.output_preparation import plot_lorentz_curve, plot_mobility_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import ks_2samp
+
+from bequestlib.couple import Couple
+from bequestlib.generation import Generation
+from bequestlib.globals import OUTFOLDER
+from bequestlib.metrics import lorentz_curve, gini
+from bequestlib.output_preparation import plot_convergence_lorentz_curve
+from bequestlib.person import Person
+from bequestlib.society_rules import (all_children_equal, male_primogeniture, love_is_blind,
+                                      best_partner_is_richest_partner)
+
 
 def init_generation():
     # set up a fully egalitarian starting generation
@@ -22,47 +24,77 @@ def init_generation():
         wf = Person(gender=False, parent_couple_id=couples-i)
         hb.set_id(i)
         wf.set_id(couples+i)
-        hb.add_inheritance(0)
-        wf.add_inheritance(0)
+        hb.add_inheritance(0.01)
+        wf.add_inheritance(0.)
         cp = Couple(husband=hb, wife=wf, c_id=i)
         couples_list.append(cp)
 
     gen_1 = Generation(g_id=1, couples=couples_list)
     return gen_1
 
-def asf_generator():
+
+def next_gen_generator(bequest_rule, marital_tradition):
     adult_gen = init_generation()
     wealths = np.sort(adult_gen.to_array()[:, 1])
     a = wealths / sum(wealths)
     test_period = 0
-    while test_period < 6:
+    tot_period = 0
+    while test_period < 3 and tot_period < 40:
         adult_gen.distribute_children()
-        next_gen = adult_gen.produce_new_generation(bequest_rule=all_children_equal,
-                                                    marital_tradition=best_partner_is_richest_partner)
-        prev_gen = adult_gen
+        next_gen = adult_gen.produce_new_generation(bequest_rule=bequest_rule,
+                                                    marital_tradition=marital_tradition)
         adult_gen = next_gen
-        x, y = lorentz_curve(adult_gen)
 
-        wealths = np.sort(adult_gen.to_array()[:, 1])
+        wealth = np.sort(adult_gen.to_array()[:, 1])
         prev_a = a
-        a = wealths / sum(wealths)
+        a = wealth / sum(wealth)
         d, p = ks_2samp(prev_a, a)
-        if p > 0.5:
+        if p > 0.2:
             test_period = test_period + 1
         else:
             test_period = 0
-        yield adult_gen
+        tot_period = tot_period + 1
+        yield adult_gen, d, p
 
 
 def main():
     if not os.path.exists(OUTFOLDER):
         os.mkdir(OUTFOLDER)
     os.chdir(OUTFOLDER)
+    mos_gen = next_gen_generator(male_primogeniture, best_partner_is_richest_partner)
+    mar_gen = next_gen_generator(all_children_equal, best_partner_is_richest_partner)
+    eq_gen = next_gen_generator(all_children_equal, love_is_blind)
+    tmp = [(adult_gen, d, p) for adult_gen, d, p in mar_gen]
+    tmp2 = [(adult_gen, d, p) for adult_gen, d, p in eq_gen]
+    tmp3 = [(adult_gen, d, p) for adult_gen, d, p in mos_gen]
+    res = (lorentz_curve(adult_gen) for adult_gen, d, p in tmp3)
+    plot_convergence_lorentz_curve(*res)
+    res2 = [gini(*lorentz_curve(adult_gen)) for adult_gen, d, p in tmp]
+    ds = [d for adult_gen, d, p in tmp]
+    ps = [p for adult_gen, d, p in tmp]
 
-    res = [lorentz_curve(adult_gen) for adult_gen in asf_generator()]
-    res2 = [gini(*y) for y in res]
-    plt.plot(res2)
+    plt.clf()
+    x1, y1 = lorentz_curve(tmp[-1][0])
+    x2, y2 = lorentz_curve(tmp2[-1][0])
+    x3, y3 = lorentz_curve(tmp3[-1][0])
+    plt.plot(x1, y1)
+    plt.plot(x2, y2)
+    plt.plot(x3, y3)
     plt.show()
+    """
+    plt.clf()
+    plt.axhline(y=0.5, color='orange')
+    plt.plot(ps)
+    plt.xlabel("Generation")
+    plt.ylabel("p-value distribution equality")
+    plt.savefig("P-value_ks_stat"+TIMESTAMP+".png")
+
+    plt.clf()
+    plt.plot(res2)
+    plt.xlabel("generation")
+    plt.ylabel("Gini coefficient")
+    plt.savefig("gini_" + TIMESTAMP + ".png")
+    """
 
 if __name__ == '__main__':
     main()
