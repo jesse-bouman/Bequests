8ecaa4f99e376e0aa2fc6848c9c6ac378b62c7f3
jesse-bouman <jesse.bouman@icloud.com>
2018-06-12 10:29:11 +0200

    fun test to get plotly in

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from bequestlib.person import Person
 from bequestlib.society_rules import (all_children_equal, male_primogeniture, love_is_blind,
                                       best_partner_is_richest_partner)
 
+from bequestlib.DEV.plots_plotly import plot_stats
 
 def init_generation():
     # set up a fully egalitarian starting generation
@@ -39,7 +40,7 @@ def next_gen_generator(bequest_rule, marital_tradition, tax_rate):
     a = wealths / sum(wealths)
     test_period = 0
     tot_period = 0
-    while test_period < 3 and tot_period < 40:
+    while test_period < 30 and tot_period < 10:
         adult_gen.distribute_children()
         next_gen = adult_gen.produce_new_generation(bequest_rule=bequest_rule,
                                                     marital_tradition=marital_tradition,
@@ -62,31 +63,21 @@ def main():
     if not os.path.exists(OUTFOLDER):
         os.mkdir(OUTFOLDER)
     os.chdir(OUTFOLDER)
-    mar_gen_0 = next_gen_generator(all_children_equal, best_partner_is_richest_partner, 0)
+
     mar_gen_2 = next_gen_generator(all_children_equal, best_partner_is_richest_partner, 0.2)
-    mar_gen_4 = next_gen_generator(all_children_equal, best_partner_is_richest_partner, 0.4)
-    mar_gen_6 = next_gen_generator(all_children_equal, best_partner_is_richest_partner, 0.6)
-    mar_gen_8 = next_gen_generator(all_children_equal, best_partner_is_richest_partner, 0.8)
-    mar_gen_10 = next_gen_generator(all_children_equal, best_partner_is_richest_partner, 1)
-    tmp_1 = [(adult_gen, d, p) for adult_gen, d, p in mar_gen_0]
-    tmp_2 = [(adult_gen, d, p) for adult_gen, d, p in mar_gen_2]
-    tmp_3 = [(adult_gen, d, p) for adult_gen, d, p in mar_gen_4]
-    tmp_4 = [(adult_gen, d, p) for adult_gen, d, p in mar_gen_6]
-    tmp_5 = [(adult_gen, d, p) for adult_gen, d, p in mar_gen_8]
-    tmp_6 = [(adult_gen, d, p) for adult_gen, d, p in mar_gen_10]
 
-    e1 = tmp_1[-1][0].time_spent_working()
-    e2 = tmp_2[-1][0].time_spent_working()
-    e3 = tmp_3[-1][0].time_spent_working()
-    e4 = tmp_4[-1][0].time_spent_working()
-    e5 = tmp_5[-1][0].time_spent_working()
-    e6 = tmp_6[-1][0].time_spent_working()
+    tmp_1 = [(adult_gen, d, p) for adult_gen, d, p in mar_gen_2]
 
-    plt.plot([0, 20, 40, 60, 80, 100], [e1, e2, e3, e4, e5, e6])
-    plt.xlabel('Tax Rate (%)')
-    plt.ylabel('Total labour supply')
-    plt.show()
-    plt.clf()
+
+    gen = tmp_1[-1][0]
+
+    e_list = [c.e for c in gen.cs]
+    w_list = [c.w for c in gen.cs]
+    c_list = [c.c for c in gen.cs]
+    lor = lorentz_curve(gen)
+    plot_stats(e_list, w_list, c_list, lor)
+
+
     # res = (lorentz_curve(adult_gen) for adult_gen, d, p in tmp3)
     #plot_convergence_lorentz_curve(*res)
     # ds = [d for adult_gen, d, p in tmp]
@@ -94,48 +85,7 @@ def main():
 
     plt.clf()
     x1, y1 = lorentz_curve(tmp_1[-1][0])
-    x2, y2 = lorentz_curve(tmp_2[-1][0])
-    x3, y3 = lorentz_curve(tmp_3[-1][0])
-    x4, y4 = lorentz_curve(tmp_4[-1][0])
-    x5, y5 = lorentz_curve(tmp_5[-1][0])
-    x6, y6 = lorentz_curve(tmp_6[-1][0])
 
-    gini_1 = gini(x1, y1)
-    gini_2 = gini(x2, y2)
-    gini_3 = gini(x3, y3)
-    gini_4 = gini(x4, y4)
-    gini_5 = gini(x5, y5)
-    gini_6 = gini(x6, y6)
-
-    plt.plot([0, 20, 40, 60, 80, 100], [gini_1, gini_2, gini_3, gini_4, gini_5, gini_6])
-    plt.xlabel('Tax Rate (%)')
-    plt.ylabel('GINI coefficient')
-    plt.show()
-    plt.clf()
-
-    plt.plot(x1, y1, label='tax rate 0%')
-    plt.plot(x2, y2, label='tax rate 20%')
-    plt.plot(x3, y3, label='tax rate 40%')
-    plt.plot(x4, y4, label='tax rate 60%')
-    plt.plot(x5, y5, label='tax rate 80%')
-    plt.plot(x6, y6, label='tax rate 100%')
-    plt.legend()
-    plt.savefig('compare_rules.png')
-    plt.show()
-    """
-    plt.clf()
-    plt.axhline(y=0.5, color='orange')
-    plt.plot(ps)
-    plt.xlabel("Generation")
-    plt.ylabel("p-value distribution equality")
-    plt.savefig("P-value_ks_stat"+TIMESTAMP+".png")
-
-    plt.clf()
-    plt.plot(res2)
-    plt.xlabel("generation")
-    plt.ylabel("Gini coefficient")
-    plt.savefig("gini_" + TIMESTAMP + ".png")
-    """
 
 
 if __name__ == '__main__':
