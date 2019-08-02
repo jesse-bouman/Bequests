66fdfdb869921a2be1fbe6c4a6c16a3fcedab788
Jesse.Bouman <jesse.bouman@rabobank.nl>
2019-08-02 13:50:11 +0200

    add functionality to reproduce

--- src/bequestlib/random.py
+++ src/bequestlib/random.py
@@ -1,10 +1,20 @@
 from numpy.random import RandomState
-from numpy.random import randint
-SEED = randint(1, 10000000)
-print(SEED)
-RANDOM_STATE = RandomState(SEED)
+
+RANDOM_STATE = None
 
 
-def get_random_state():
+def get_random_state() -> RandomState:
     global RANDOM_STATE
+    if not RANDOM_STATE:
+        RANDOM_STATE = RandomState()
     return RANDOM_STATE
+
+
+def set_random_state_with_seed(seed: int):
+    global RANDOM_STATE
+    RANDOM_STATE = RandomState(seed)
+
+
+def get_random_state_seed():
+    state = get_random_state()
+    return state.seed()
